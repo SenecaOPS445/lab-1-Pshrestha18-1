da5ef51ff05ee6bef9a94f7b944a032fa900c767
Pshrestha18 <156871799+Pshrestha18@users.noreply.github.com>
2025-01-17 18:00:28 -0500

    lab1b completed

---
+++ lab1b.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+
+'''Description: This program will output "How old are you Isaac?".'''
+
+name =  'Isaac'
+print('How old are you ' + name + '?' )
---
+++ lab1c.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+'''
+Description: This program will output "Isaac is 72 years old!".
+'''
+
+name = 'Isaac'
+age = 72
+
+# Print the required output
+print(f'{name} is {age} years old!')
